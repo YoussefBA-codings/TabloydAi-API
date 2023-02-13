798adfd889749ba11f482db7abaa7d883a614877
qbekkai <quentin.bekkai@gmail.com>
2023-02-13 16:34:23 +0100

    fix

--- config/firebase-config.ts
+++ config/firebase-config.ts
@@ -15,17 +15,4 @@ const firebaseConfig = {
 	clientX509CertUrl: process.env.CLIENT_X509_CERT_URL,
 };
 
-// const firebaseConfig = {
-// 	type: serviceAccount.type,
-// 	project_id: serviceAccount.project_id,
-// 	private_key_id: serviceAccount.private_key_id,
-// 	private_key: serviceAccount.private_key,
-// 	client_email: serviceAccount.client_email,
-// 	client_id: serviceAccount.client_id,
-// 	auth_uri: serviceAccount.auth_uri,
-// 	token_uri: serviceAccount.token_uri,
-// 	auth_provider_x509_cert_url: serviceAccount.auth_provider_x509_cert_url,
-// 	client_x509_cert_url: serviceAccount.client_x509_cert_url,
-// };
-
 export default firebaseConfig;
