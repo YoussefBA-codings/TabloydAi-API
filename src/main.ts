582209ebac26bc4b9865debca310ff2d0f994c78
qbekkai <quentin.bekkai@gmail.com>
2023-03-08 10:02:34 +0100

    resolve conflict + delete upload folder

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,7 @@ import { AppModule } from '@/app.module';
 import { PrismaService } from '@/services/prisma.service';
 
 async function bootstrap() {
-<<<<<<< HEAD
-	const app = await NestFactory.create(AppModule, {
-	});
-=======
 	const app = await NestFactory.create(AppModule);
->>>>>>> 4df5aa542bd8faadb682713de4ff596ffc77bcb0
 	const prismaService = app.get(PrismaService);
 	await prismaService.enableShutdownHooks(app);
 	await app.listen(3000);
